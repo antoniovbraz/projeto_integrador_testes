1826ec6fa5700846d6d913035e5ab3943074e0eb
Antonio Braz <antoniovbraz@gmail.com>
2023-02-15 17:52:35 -0300

    Started creating the CSS base rules and structuring the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      {/* ==== header top ==== */};
+      <div className="header_top">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6">
+              <div className="header_top_left">
+                <span>Need Help?</span>
+                <span className="header_top_left">
+                  <i class="ri-phone-fill"></i> +55 11 97438-2509
+                </span>
+              </div>
+            </Col>
+            <Col lg="6" md="6" sm="6">
+              <div className="header_top_right">
+                <Link to="#">
+                  <i class="ri-login-circle-line"></i> Login
+                </Link>
+
+                <Link to="#">
+                  <i class="ri-user-line"></i> Register
+                </Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
