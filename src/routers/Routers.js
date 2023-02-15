2a3eb9117a77c2b630a7959fa586ff752a186c09
Antonio Braz <antoniovbraz@gmail.com>
2023-02-15 14:37:48 -0300

    Finished the routes structure

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -9,7 +9,18 @@ import BlogDetails from "../pages/BlogDetails";
 import NotFound from "../pages/NotFound";
 
 const Routers = () => {
-  return <div>Routers</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/cars" element={<CarListing />} />
+      <Route path="/cars/:slug" element={<CarDetails />} />
+      <Route path="/blogs" element={<Blog />} />
+      <Route path="/blogs/:slug" element={<BlogDetails />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default Routers;
